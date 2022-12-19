7c0753fc136c326cce06f0ff66e5c5857c178f9b
Ahmad Syauqi <syauqiahmd@Ahmads-MacBook-Pro.local>
2022-12-19 13:49:36 +0800

    fix(env): update env conf

--- config/redis.js
+++ config/redis.js
@@ -1,14 +1,13 @@
 const Redis = require('ioredis');
-const fs = require('fs');
 
-// const redis = new Redis({
-//     host: 'redis-14489.c54.ap-northeast-1-2.ec2.cloud.redislabs.com',
-//     port: 14489,
-//     password: 'kBJ1QM71st0DbSvw2VFnXyxTQVSKOt5S'
-// });
 const redis = new Redis({
-    host: '127.0.0.1',
-    port: 6379
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD
 });
+// const redis = new Redis({
+//     host: '127.0.0.1',
+//     port: 6379
+// });
 
 module.exports = redis
